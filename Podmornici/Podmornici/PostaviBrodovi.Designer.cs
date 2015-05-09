a32bfc72de7008116c3b3141f95fa4ec639c9f32
m1ch0 <milancotajanovski@gmail.com>
2015-05-09 14:27:19 +0200

    dodadeni zvuci 1

---
+++ Podmornici/Podmornici/PostaviBrodovi.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Podmornici
+{
+    partial class PostaviBrodovi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPocni = new System.Windows.Forms.Button();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnResetiraj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPocni
+            // 
+            this.btnPocni.Location = new System.Drawing.Point(570, 269);
+            this.btnPocni.Name = "btnPocni";
+            this.btnPocni.Size = new System.Drawing.Size(142, 35);
+            this.btnPocni.TabIndex = 0;
+            this.btnPocni.Text = "Започни игра";
+            this.btnPocni.UseVisualStyleBackColor = true;
+            this.btnPocni.Click += new System.EventHandler(this.btnPocni_Click);
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(570, 355);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(142, 35);
+            this.btnRandom.TabIndex = 1;
+            this.btnRandom.Text = "Намести случајно";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnResetiraj
+            // 
+            this.btnResetiraj.Location = new System.Drawing.Point(570, 314);
+            this.btnResetiraj.Name = "btnResetiraj";
+            this.btnResetiraj.Size = new System.Drawing.Size(142, 35);
+            this.btnResetiraj.TabIndex = 2;
+            this.btnResetiraj.Text = "Ресетирај";
+            this.btnResetiraj.UseVisualStyleBackColor = true;
+            this.btnResetiraj.Click += new System.EventHandler(this.btnResetiraj_Click);
+            // 
+            // PostaviBrodovi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 402);
+            this.Controls.Add(this.btnResetiraj);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.btnPocni);
+            this.Name = "PostaviBrodovi";
+            this.Text = "PostaviBrodovi";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PostaviBrodovi_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PostaviBrodovi_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PostaviBrodovi_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PostaviBrodovi_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PostaviBrodovi_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPocni;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button btnResetiraj;
+    }
+}
---
+++ Podmornici/Podmornici/PostaviBrodovi.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Podmornici
+{
+    public partial class PostaviBrodovi : Form
+    {
+        private bool pritisnatMaus;
+        private int prevX;
+        private int prevY;
+        Brod selektiran;
+        bool Nivo;
+        String Ime;
+        MapaIgrach mapaIgrach { get; set; }
+        public PostaviBrodovi(String ime, bool nivo)
+        {
+
+            InitializeComponent();
+            Ime = ime;
+            Nivo = nivo;
+            pritisnatMaus = false;
+  
+            DoubleBuffered = true;
+            namestiOsnovno();
+            Invalidate();
+        }
+
+        private void namestiOsnovno()
+        {
+            mapaIgrach = new MapaIgrach();
+           
+            mapaIgrach.Brodovi.Add(new Brod(3, Brod.Nasoka.Horizontalno, 2, 12));
+            mapaIgrach.Brodovi.Add(new Brod(4, Brod.Nasoka.Horizontalno, 4, 12));
+            mapaIgrach.Brodovi.Add(new Brod(5, Brod.Nasoka.Horizontalno, 6, 12));
+            mapaIgrach.Brodovi.Add(new Brod(2, Brod.Nasoka.Horizontalno, 0, 12));
+        }
+        private void PostaviBrodovi_MouseDown(object sender, MouseEventArgs e)
+        {
+            pritisnatMaus = true;
+            
+            foreach (Brod b in mapaIgrach.Brodovi)
+            {
+
+                Console.WriteLine(b.ToString());
+
+                if (b.selektiranSum(e.X, e.Y))
+                {
+                    selektiran = b;
+                    break;
+                }
+            }
+            prevX = e.X;
+            prevY = e.Y;
+        }
+
+        private void PostaviBrodovi_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (selektiran != null) {
+                mapaIgrach.dodajBrodNaMapa(selektiran);
+                pritisnatMaus = false;
+                Point p = selektiran.voPiksel(selektiran.X, selektiran.Y);
+                selektiran.pikselX = p.X;
+                selektiran.pikselY = p.Y;
+                selektiran = null;
+            }
+            
+        }
+
+        private void PostaviBrodovi_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pritisnatMaus)
+            {
+                if (selektiran != null)
+                {
+                    int dx = e.X - prevX;
+                    int dy = e.Y - prevY;
+                    selektiran.Pomesti(dx, dy);
+                    Invalidate();
+                }
+                prevX = e.X;
+                prevY = e.Y;
+            }      
+        }
+
+        private void PostaviBrodovi_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            foreach (Brod b in mapaIgrach.Brodovi)
+            {
+                b.crtaj(g, true);
+            }
+
+            int x = 25;
+            int y = 50;
+            Pen moliv = new Pen(Color.Chocolate);
+            for (int i = 0; i < 11; i++)
+            {
+                g.DrawLine(moliv, x + i * 30, y, x + i * 30, 350);
+                g.DrawLine(moliv, 25, y + i * 30, 325, y + i * 30);
+
+                //g.DrawLine(moliv, 360 + x + i * 30, y, 360 + x + i * 30, 350);
+                //g.DrawLine(moliv, 385, y + i * 30, 685, y + i * 30);
+
+             
+            }
+            moliv.Dispose();
+            mapaIgrach.crtaj(g);
+        }
+
+        private void PostaviBrodovi_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                foreach (Brod b in mapaIgrach.Brodovi)
+                {
+                    if (b.selektiranSum(e.X, e.Y))
+                    {
+                        b.smeniNasoka();
+                        Invalidate();
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void btnResetiraj_Click(object sender, EventArgs e)
+        {
+            namestiOsnovno();
+            Invalidate();
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+
+            mapaIgrach.napolniSlucajno();
+            Invalidate();
+        }
+
+        private void btnPocni_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9 ; j++)
+                {
+                    Console.Write(mapaIgrach.mapa[i][j].ToString());
+                }
+                Console.WriteLine();
+            }
+            Form1 n = new Form1(Ime,mapaIgrach);
+            n.Show();
+        }
+    }
+}
